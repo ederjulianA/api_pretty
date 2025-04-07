d5d7df01348e9bfbbae56c5ea27efb1e76d1528f
ederjulianA <ederalvarez2091057@gmail.com>
2025-04-07 13:20:09 -0500

    Actualización rutas articulos

--- routes/articulosRoutes.js
+++ routes/articulosRoutes.js
@@ -8,7 +8,7 @@ const verifyToken = require('../middlewares/auth'); // Importar el middleware
 const router = express.Router();
 const articulosController = require('../controllers/articulosController');
 const { validateArticuloEndpoint } = require('../controllers/articulosController');
-const { createArticuloEndpoint, getArticuloEndpoint , updateArticuloEndpoint   } = require('../controllers/articulosController');
+const { createArticuloEndpoint, getArticuloEndpoint , updateArticuloEndpoint, getArticuloByArtCodEndPoint } = require('../controllers/articulosController');
 // Endpoint GET para consulta de artículos
 // Ejemplo de uso:
 // GET /api/articulos?nombre=rubyface&inv_gru_cod=9&PageNumber=1&PageSize=50
@@ -21,5 +21,6 @@ router.post('/', createArticuloEndpoint);
 
 router.get('/:id_articulo', getArticuloEndpoint);
 router.put('/:id_articulo', updateArticuloEndpoint);
+router.get('/articulo/:art_cod', getArticuloByArtCodEndPoint);
 
 module.exports = router;
